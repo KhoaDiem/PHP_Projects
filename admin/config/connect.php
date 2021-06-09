f8466263d68a955179a4bb6ceeb9f2f5d95d2b6d
github_KhoaDiem <diemtrong01@gmail.com>
2021-06-09 16:39:52 +0700

    abc

--- admin/config/connect.php
+++ admin/config/connect.php
@@ -1,8 +1,11 @@
 <?php
-$conn = mysqli_connect('34.206.113.91','khoadiem','khoadiem@123','demo');
-if(!$conn){
-	echo"ket noi that bai";
-}else{
-	echo "";
-}
+		$hostname = "34.206.113.91";
+		$user = "khoadiem";
+		$pass = "khoadiem@123";
+		$db = "demo";
+
+		$con = mysqli_connect($hostname,$user,$pass,$db);
+		mysqli_query($con,$db);
+		mysqli_set_charset($con,"utf8");
+
 ?>
