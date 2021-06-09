70e86226c2d35ac96cf8a8b6d7071d88dcc3a441
github_KhoaDiem <diemtrong01@gmail.com>
2021-06-09 16:05:22 +0700

    abc

--- admin/config/connect.php
+++ admin/config/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('34.206.113.91','KhoaDiem','khoadiem@123','KhoaDiem');
+$conn = mysqli_connect('34.206.113.91','khoadiem','khoadiem@123','KhoaDiem');
 if(!$conn){
 	echo"ket noi that bai";
 }else{
