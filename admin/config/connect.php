2fb766f3004f23a7ca224a9634c846a5a53eeb89
github_KhoaDiem <diemtrong01@gmail.com>
2021-06-16 14:43:09 +0700

    make it better

--- admin/config/connect.php
+++ admin/config/connect.php
@@ -1,8 +1,8 @@
 <?php
-		$hostname = "52.6.114.59";
-		$user = "diem";
-		$pass = "diem1234";
-		$db = "diem";
+		$hostname = "localhost";
+		$user = "root";
+		$pass = "";
+		$db = "demo";
 
 		$con = mysqli_connect($hostname,$user,$pass,$db);
 		mysqli_query($con,$db);
